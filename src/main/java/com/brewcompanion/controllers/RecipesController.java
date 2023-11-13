284ee09008d286918dea1c904710f9711eff05ac
Alex <17982529+alexm622@users.noreply.github.com>
2023-11-13 09:09:11 -0500

    recipe page and some messing around with dropdowns

--- src/main/java/com/brewcompanion/controllers/RecipesController.java
+++ src/main/java/com/brewcompanion/controllers/RecipesController.java
@@ -1,5 +1,6 @@
 package com.brewcompanion.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -17,13 +18,30 @@ public class RecipesController {
 	//landing page
 	@GetMapping("")
 	public String recipes(@RequestParam(name="page", required=false, defaultValue="1") Integer page, Model model) {
-        List<Recipe> recipes = Recipe.getExampleData();
+        List<Recipe> recipes = Recipe.getRandomData(12);
         //log the recipes
         for (Recipe recipe : recipes) {
             System.out.println(recipe.getName());
         }
-        model.addAttribute("recipes", recipes);
+        model.addAttribute("recipetable", toTableView(recipes));
         return "recipes";
+    }
+
+    private List<List<Recipe>> toTableView(List<Recipe> recipes) {
+        //make a 3 column table
+        List<List<Recipe>> table = new ArrayList<>();
+        int i = 0;
+        while (i < recipes.size()) {
+            List<Recipe> row = new ArrayList<>();
+            for (int j = 0; j < 3; j++) {
+                if (i < recipes.size()) {
+                    row.add(recipes.get(i));
+                    i++;
+                }
+            }
+            table.add(row);
+        }
+        return table;
     }
 
 
