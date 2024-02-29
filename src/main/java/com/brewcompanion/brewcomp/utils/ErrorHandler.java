f16ebf5882d5f470fcffcb2a38b03319ffa31daa
Alex <17982529+alexm622@users.noreply.github.com>
2024-02-28 19:04:39 -0500

    started work on error handling

--- src/main/java/com/brewcompanion/brewcomp/utils/ErrorHandler.java
+++ src/main/java/com/brewcompanion/brewcomp/utils/ErrorHandler.java
@@ -2,9 +2,11 @@ package com.brewcompanion.brewcomp.utils;
 
 import java.util.Date;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -17,6 +19,18 @@ public class ErrorHandler {
         mav.addObject("exception", ex);
         mav.addObject("url", req.getRequestURL());
         mav.addObject("timestamp", new Date());
+        mav.setViewName("error");
+        return mav;
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ModelAndView handleResourceNotFound(HttpServletRequest req, Exception ex) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("message", "Resource not found");
+        mav.addObject("url", req.getRequestURL());
+        mav.addObject("timestamp", new Date());
+        mav.addObject("code", 404);
+        mav.setStatus(HttpStatusCode.valueOf(404));
         mav.setViewName("error");
         return mav;
     }
